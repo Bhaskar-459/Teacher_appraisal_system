7257ebeb69d5f31d499355f180296b66ca9a1ccb
Bhaskar-459 <m03bhaskar@gmail.com>
2024-09-18 17:14:48 +0530

    changed pdf code to fetch from database

--- backend/routes/pdfRoutes.js
+++ backend/routes/pdfRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const { generatePDF } = require('../controllers/pdfController');
 
 // Generate PDF
-router.post('/generatePdf', generatePDF);
+router.post('/generatePdf/:id', generatePDF);
 
 module.exports = router;
